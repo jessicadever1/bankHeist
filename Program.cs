e90b264f5edc6fe3835608537a3f15e1ff4d4062
Jess Dever <jessicadever1@gmail.com>
2021-03-08 16:00:26 -0600

    Phase 1

--- Program.cs
+++ Program.cs
@@ -7,6 +7,21 @@ namespace bankHeist
         static void Main(string[] args)
         {
             Console.WriteLine("Plan Your Heist!");
+
+            Console.WriteLine("Let's register your team. What is a team player's name?");
+            string nameInput = Console.ReadLine();
+
+            Console.WriteLine($"Great! What is {nameInput}'s skill level?");
+            string skillLevelInput = Console.ReadLine();
+            int skillLevel = Int32.Parse(skillLevelInput);
+
+            Console.WriteLine($"And what is {nameInput}'s courage factor (on a scale of 0.0 to 2.0)?");
+            string CourageFactorInput = Console.ReadLine();
+            double CourageFactor = Int32.Parse(CourageFactorInput);
+
+            Console.WriteLine($"Team Member: {nameInput}");
+            Console.WriteLine($"Skill Level: {skillLevel}");
+            Console.WriteLine($"Courage Factor: {CourageFactor}");
         }
     }
 }
---
+++ TeamMember.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace bankHeist
+{
+    public class TeamMember
+    {
+        public string Name { get; set; }
+        public int SkillLevel { get; set; }
+        public double CourageFactor { get; set; }
+    }
+}
